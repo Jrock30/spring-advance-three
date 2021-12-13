eedc970b39a1aa0fbf2f12c335c382d8490190a7
Jrock30 <jrg8989@knou.ac.kr>
2021-12-13 14:54:20 +0900

    Initialize

---
+++ src/main/java/hello/aop/SpringAdvanceThreeApplication.java
@@ -0,0 +1,13 @@
+package hello.aop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAdvanceThreeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAdvanceThreeApplication.class, args);
+    }
+
+}
